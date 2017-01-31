f467d97cfe9cc64adfc42c5762e45a24b232e0a3
jamiemchugh9 <jamiemchugh7@gmail.com>
2017-01-31 14:52:29 +0000

    lab not fully done

--- week2/Driver.java
+++ week2/Driver.java
@@ -1,18 +1,2 @@
 public class Driver {
-
-	
-
-	/**
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-Cart testCart;
-		CartItem c = new CartItem();
-		testCart = new Cart(1,c);
-		testCart.print();
-
-		
-	}
-
 }
